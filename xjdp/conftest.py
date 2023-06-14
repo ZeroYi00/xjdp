fe7d3db8f6d7b798a6e0efc6d14b8ab5695b031c
Yaxin <yaxinyeang@gmail.com>
2023-06-15 00:44:29 +0800

    first commit

---
+++ xjdp/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from xjdp.users.models import User
+from xjdp.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
